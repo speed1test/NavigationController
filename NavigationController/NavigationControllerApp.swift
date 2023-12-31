03380749b95b10815f14bceaf5278465725fee70
Edwin <edwin12sv@gmail.com>
2023-12-30 18:06:18 -0600

    Initial Commit

---
+++ NavigationController/NavigationControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationControllerApp.swift
+//  NavigationController
+//
+//  Created by elsalvador on 30/12/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
